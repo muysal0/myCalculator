e34ec3e6c96139f54e593c5730266404163eb318
muysal0 <muysal0103@gmail.com>
2026-07-14 10:49:18 +0300

    Initial Commit

---
+++ myCalculator/myCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCalculatorApp.swift
+//  myCalculator
+//
+//  Created by Mehmet UYSAL on 14.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct myCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
